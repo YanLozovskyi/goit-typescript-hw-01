b07a1160a1086474de1336465ee99b16b0d6dc21
YanLozovskyi <yanlozovskiy@gmail.com>
2023-10-26 17:48:37 +0300

    completed homework

---
+++ src/test.ts
@@ -0,0 +1,37 @@
+//  Примитивы
+
+// let total: number = 5;
+// let userName: string = "igor";
+// let isActive: boolean = false;
+// let empty: null = null;
+// let undf: undefined = undefined;
+
+// total = "five";
+
+// Сложные типы
+
+// Array
+
+// const numbers: number[] = [1, 2, 3, 4, 5, 6];
+// numbers.push("12");
+
+// Object
+
+// const user: { name: string; age: number } = {
+//   name: "Igor",
+//   age: 12,
+// };
+
+// user.age = "15";
+// user.name = 1;
+
+// type User = { name: string; age: number };
+
+// const user: User = { name: "Igor", age: 12 };
+
+// user.age = "15";
+// user.name = 1;
+
+// type eventType = "lesson" | "deadline";
+
+// const userEvent: eventType = "homework";
